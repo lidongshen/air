0fd5b241bd069e8dab0a84b542507e9a78d2d1b1
hzhgit <905485089@qq.com>
2019-11-13 08:46:32 +0800

    [修改] 	User实体类

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -9,89 +9,108 @@ import java.util.Date;
  */
 public class User {
 	private int uId;
-	private Date uAge;	
-	private int oId;
+	private int uNum;
 	private String uPhone;
 	private String uPassword;
 	private String uName;
+	private int uAge;	
 	private String uSex;
-	public User(int uId, Date uAge, int oId, String uPhone, String uPassword, String uName, String uSex) {
+	
+	public User(int uId, int uNum, String uPhone, String uPassword, String uName, int uAge, String uSex) {
 		super();
 		this.uId = uId;
-		this.uAge = uAge;
-		this.oId = oId;
+		this.uNum = uNum;
 		this.uPhone = uPhone;
 		this.uPassword = uPassword;
 		this.uName = uName;
+		this.uAge = uAge;
 		this.uSex = uSex;
 	}
-	public User(Date uAge, int oId, String uPhone, String uPassword, String uName, String uSex) {
+
+	public User(int uNum, String uPhone, String uPassword, String uName, int uAge, String uSex) {
 		super();
-		this.uAge = uAge;
-		this.oId = oId;
+		this.uNum = uNum;
 		this.uPhone = uPhone;
 		this.uPassword = uPassword;
 		this.uName = uName;
+		this.uAge = uAge;
 		this.uSex = uSex;
 	}
+
 	public User() {
 		super();
 	}
+
 	public int getuId() {
 		return uId;
 	}
+
 	public void setuId(int uId) {
 		this.uId = uId;
 	}
-	public Date getuAge() {
-		return uAge;
+
+	public int getuNum() {
+		return uNum;
 	}
-	public void setuAge(Date uAge) {
-		this.uAge = uAge;
+
+	public void setuNum(int uNum) {
+		this.uNum = uNum;
 	}
-	public int getoId() {
-		return oId;
-	}
-	public void setoId(int oId) {
-		this.oId = oId;
-	}
+
 	public String getuPhone() {
 		return uPhone;
 	}
+
 	public void setuPhone(String uPhone) {
 		this.uPhone = uPhone;
 	}
+
 	public String getuPassword() {
 		return uPassword;
 	}
+
 	public void setuPassword(String uPassword) {
 		this.uPassword = uPassword;
 	}
+
 	public String getuName() {
 		return uName;
 	}
+
 	public void setuName(String uName) {
 		this.uName = uName;
 	}
+
+	public int getuAge() {
+		return uAge;
+	}
+
+	public void setuAge(int uAge) {
+		this.uAge = uAge;
+	}
+
 	public String getuSex() {
 		return uSex;
 	}
+
 	public void setuSex(String uSex) {
 		this.uSex = uSex;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + oId;
-		result = prime * result + ((uAge == null) ? 0 : uAge.hashCode());
+		result = prime * result + uAge;
 		result = prime * result + uId;
 		result = prime * result + ((uName == null) ? 0 : uName.hashCode());
+		result = prime * result + uNum;
 		result = prime * result + ((uPassword == null) ? 0 : uPassword.hashCode());
 		result = prime * result + ((uPhone == null) ? 0 : uPhone.hashCode());
 		result = prime * result + ((uSex == null) ? 0 : uSex.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -101,12 +120,7 @@ public class User {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		if (oId != other.oId)
-			return false;
-		if (uAge == null) {
-			if (other.uAge != null)
-				return false;
-		} else if (!uAge.equals(other.uAge))
+		if (uAge != other.uAge)
 			return false;
 		if (uId != other.uId)
 			return false;
@@ -114,6 +128,8 @@ public class User {
 			if (other.uName != null)
 				return false;
 		} else if (!uName.equals(other.uName))
+			return false;
+		if (uNum != other.uNum)
 			return false;
 		if (uPassword == null) {
 			if (other.uPassword != null)
@@ -132,10 +148,11 @@ public class User {
 			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
-		return "User [uId=" + uId + ", uAge=" + uAge + ", oId=" + oId + ", uPhone=" + uPhone + ", uPassword="
-				+ uPassword + ", uName=" + uName + ", uSex=" + uSex + "]";
+		return "User [uId=" + uId + ", uNum=" + uNum + ", uPhone=" + uPhone + ", uPassword=" + uPassword + ", uName="
+				+ uName + ", uAge=" + uAge + ", uSex=" + uSex + "]";
 	}
 	
 	
